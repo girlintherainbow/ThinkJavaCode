96ff23aa140d45866975fe6b4c0f0890fcffd079
glowo <gloworm93@gmail.com>
2018-01-18 14:34:34 -0600

    Exercise 2.2

---
+++ ch02/Date.java
@@ -0,0 +1,25 @@
+public class Date
+{
+    public static void main(String[] args)
+    {
+        String day = "Thursday";
+        int date = 18;
+        String month = "January ";
+        int year = 2018;
+
+        System.out.print(day);
+        System.out.print(", ");
+        System.out.print(month);
+        System.out.print(date);
+        System.out.print(", ");
+        System.out.println(year);
+
+        System.out.print(day);
+        System.out.print(" ");
+        System.out.print(date);
+        System.out.print(" ");
+        System.out.print(month);
+        System.out.print(year);
+
+    }
+}
