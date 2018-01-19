322414218c20feebad3c0ba2f56182a8f9281c54
glowo <gloworm93@gmail.com>
2018-01-19 16:07:54 -0600

    HW Exercises 4-2a and 4-2b

---
+++ ch04/SimpleMethods.java
@@ -0,0 +1,23 @@
+public class SimpleMethods
+{
+    public static void main(String[] args)
+    {
+        printCount(5);
+
+        printSum(4,6);
+
+        printSum(7,2);
+
+    }
+
+    public static void printCount(int count)
+    {
+        System.out.println("This count is: " + count);
+
+    }
+
+    public static void printSum(int a, int b)
+    {
+        System.out.println(a + b);
+    }
+}
