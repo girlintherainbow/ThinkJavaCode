3c4bc85eef9a1930d9ff5605600664a85fac5089
glowo <gloworm93@gmail.com>
2018-01-18 20:52:43 -0600

    HW Exercise 2-7 Partial

---
+++ ch02/Withdrawal.java
@@ -0,0 +1,19 @@
+public class Withdrawal
+{
+    public static void main(String[] args)
+    {
+        int withdrawal = 137;
+
+        int a = 20;
+        int b = 10;
+        int c = 5;
+        int d = 1;
+
+        System.out.println("Number of $20's : " + withdrawal / a );
+
+        int numberOf20s = withdrawal / a;
+
+        int remainder1 = 37;
+
+    }
+}
