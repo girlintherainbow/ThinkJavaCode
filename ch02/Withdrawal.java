22b4080fc23106e56de99cc37a0bb12380c94c01
glowo <gloworm93@gmail.com>
2018-01-19 09:07:23 -0600

    HW Exercise 2-7

--- ch02/Withdrawal.java
+++ ch02/Withdrawal.java
@@ -9,11 +9,30 @@ public class Withdrawal
         int c = 5;
         int d = 1;
 
+        System.out.println("Ideal Withdrawal Amounts: ");
+
         System.out.println("Number of $20's : " + withdrawal / a );
 
         int numberOf20s = withdrawal / a;
 
-        int remainder1 = 37;
+        int remainder1 = 17;
+
+        int numberOf10s = remainder1 / b;
+
+        System.out.println("Number or $10's: " + numberOf10s);
+
+        int remainder2 = 7;
+
+        int numberOf5s = remainder2 / c ;
+
+        System.out.println("Number of $5's: " + numberOf5s);
+
+        int remainder3 = 2;
+
+        int numberOf1s = remainder3 / 1;
+
+        System.out.println("Number of $1's: " + numberOf1s);
+
 
     }
 }
