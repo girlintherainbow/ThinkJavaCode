4b2466b73f1968d430dfc844b9675a96e4fc14ee
glowo <gloworm93@gmail.com>
2018-01-18 20:03:30 -0600

    Exercise 2.2

---
+++ ch02/DoubleByZero.java
@@ -0,0 +1,11 @@
+public class DoubleByZero
+{
+    public static void main(String[] args)
+    {
+        double a = 42;
+        double b = 0;
+        double result = a / b;
+
+        System.out.println(result);
+    }
+}
